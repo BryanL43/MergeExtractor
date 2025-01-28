9b52daf4f570fb44dba3a8a755cfc7aea39857b2
bryan lee <bryanlee56098@gmail.com>
2025-01-27 22:30:45 -0800

    Prototype openai section extraction & scaffolding project structure

---
+++ src/Assistant.py
@@ -0,0 +1,6 @@
+class Assistant:
+    def __init__(self, name: str):
+        self.name = name
+
+    def greet(self):
+        return f"Hello, {self.name}!"
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from Assistant import Assistant
+
+def main():
+    myAssistant = Assistant("Bryan")
+    print(myAssistant.greet())
+
+if __name__ == "__main__":
+    main();
