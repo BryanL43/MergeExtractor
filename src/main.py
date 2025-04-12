44f9b2d94e37733a01393001044a196ea817ad4f
bryan lee <bryanlee56098@gmail.com>
2025-04-12 01:28:45 -0700

    Shed unused package

--- src/main.py
+++ src/main.py
@@ -69,23 +69,23 @@ def main():
     
     backup_assistant = BackupAssistant(openai_api_key, "Backup Assistant", "gpt-4o-mini");
 
-    crawler = Crawler(filed_date, company_A_list, company_B_list, start_phrases, thread_pool, nlp, backup_assistant);
-    crawler.runCrawler(index=0, date_margin=4);
+    # crawler = Crawler(filed_date, company_A_list, company_B_list, start_phrases, thread_pool, nlp, backup_assistant);
+    # crawler.runCrawler(index=0, date_margin=4);
     # crawler.runCrawler(start_index=0, end_index=49, date_margin=4);
 
     analysis_assistant = AnalysisAssistant(openai_api_key, "Analysis Assistant", "gpt-4o-mini");
 
-    # initiatorClassifier = InitiatorClassifier(
-    #     openai_api_key, 
-    #     company_A_list, 
-    #     company_B_list, 
-    #     start_phrases, 
-    #     thread_pool, 
-    #     nlp, 
-    #     reranker_model, 
-    #     analysis_assistant
-    # );
-    # initiatorClassifier.findInitiator(index=8);
+    initiatorClassifier = InitiatorClassifier(
+        openai_api_key, 
+        company_A_list, 
+        company_B_list, 
+        start_phrases, 
+        thread_pool, 
+        nlp, 
+        reranker_model, 
+        analysis_assistant
+    );
+    initiatorClassifier.findInitiator(index=8);
     # initiatorClassifier.findInitiator(start_index=0, end_index=49);
 
     thread_pool.shutdown(wait=True);
