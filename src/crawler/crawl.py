cb812a1d70a13675c3744db3ecc48a9156f2a14d
Bryan Lee <bryanlee56098@gmail.com>
2025-06-15 00:47:40 -0700

    1st thousand crawled data

--- src/crawler/crawl.py
+++ src/crawler/crawl.py
@@ -2,8 +2,8 @@ from src.crawler.CrawlerHandler import CrawlerHandler
 
 def main():
     crawler = CrawlerHandler();
-    crawler.runCrawler(index=443, date_margin=4);
-    # crawler.runCrawler(start_index=400, end_index=499, date_margin=4, batch_size=5);
+    # crawler.runCrawler(index=443, date_margin=4);
+    crawler.runCrawler(start_index=900, end_index=999, date_margin=4, batch_size=5);
 
 
 if __name__ == "__main__":
