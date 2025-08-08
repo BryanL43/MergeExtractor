cf617ace0f006ff824a11403aaef1a5860134c9e
Bryan Lee <bryanlee56098@gmail.com>
2025-08-08 01:56:46 -0700

    Finished seperation stage

--- src/seperator/seperate.py
+++ src/seperator/seperate.py
@@ -3,7 +3,7 @@ from src.seperator.SeperatorHandler import SeperatorHandler
 def main():
     seperator = SeperatorHandler();
     # seperator.runSeperator(index=560);
-    seperator.runSeperator(start_index=900, end_index=999, batch_size=3);
+    seperator.runSeperator(start_index=1700, end_index=1701, batch_size=2);
 
 
 if __name__ == "__main__":
